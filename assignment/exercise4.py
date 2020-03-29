6ce08b3b12457aeb0b49c71b02131db216f4df0f
jitasinee <61464837+jitasinee@users.noreply.github.com>
2020-03-29 21:43:38 +0700

    exercise4

---
+++ assignment/exercise4.py
@@ -0,0 +1,8 @@
+Foundation=83
+General=17.4
+Introduction=38.5
+Programing=50
+print("Foundation English:",Foundation)
+print("General Bussiness:",General)
+print("Introduction to computer System:",Introduction)
+print("Computer Programing:",Programing)
