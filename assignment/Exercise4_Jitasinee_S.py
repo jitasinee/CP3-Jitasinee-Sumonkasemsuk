2f57c9344e182e6450923e267ffd09ea173ba7a1
jitasinee <61464837+jitasinee@users.noreply.github.com>
2020-03-29 21:55:26 +0700

    Create_exercise4_jitasinee

---
+++ assignment/Exercise4_Jitasinee_S.py
@@ -0,0 +1,8 @@
+english=83
+bussiness=17.4
+system=38.5
+programing=50
+print("Foundation English:",english)
+print("General Bussiness:",bussiness)
+print("Introduction to computer System:",system)
+print("Computer Programing:",programing)
