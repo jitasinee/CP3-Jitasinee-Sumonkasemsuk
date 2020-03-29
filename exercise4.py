734f32fd6e8b5ce0852c4781393412c2bc63234c
jitasinee <61464837+jitasinee@users.noreply.github.com>
2020-03-29 21:46:52 +0700

    update

--- exercise4.py
+++ exercise4.py
@@ -1,8 +1,8 @@
-Foundation=83
-General=17.4
-Introduction=38.5
-Programing=50
-print("Foundation English:",Foundation)
-print("General Bussiness:",General)
-print("Introduction to computer System:",Introduction)
-print("Computer Programing:",Programing)
+english=83
+bussiness=17.4
+system=38.5
+programing=50
+print("Foundation English:",english)
+print("General Bussiness:",bussiness)
+print("Introduction to computer System:",system)
+print("Computer Programing:",programing)
